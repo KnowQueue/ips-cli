e9a0c775a38e5ec09ac864bf4c7971095976b464
Phuoc Do <binanhphuoc@gmail.com>
2019-12-30 13:33:35 -0500

    Done deploying

--- index.js
+++ index.js
@@ -11,18 +11,52 @@ let config = JSON.parse(fs.readFileSync(config_path));
 
 let kb_path = path.isAbsolute(config.KB_MODEL) ? config.KB_MODEL : path.join(working_dir, config.KB_MODEL);
 
-if (process.argv[2] === "deploy") {
-    var formData = new FormData();
-    let filePath = path.join(kb_path, "concepts/LINE_SEGMENT.json");
-    formData.append("concepts/LINE_SEGMENT.json", fs.createReadStream(filePath));
-    const formHeaders = formData.getHeaders();
-    axios.put(config.KB_HOST+"/deploy", formData, {
-        headers: {
-            ...formHeaders
+function walk(dir) {
+    return new Promise((resolve, reject) => {
+      fs.readdir(dir, (error, files) => {
+        if (error) {
+          return reject(error);
         }
+        Promise.all(files.map((file) => {
+          return new Promise((resolve, reject) => {
+            const filepath = path.join(dir, file);
+            fs.stat(filepath, (error, stats) => {
+              if (error) {
+                return reject(error);
+              }
+              if (stats.isDirectory()) {
+                walk(filepath).then(resolve);
+              } else if (stats.isFile()) {
+                resolve(filepath);
+              }
+            });
+          });
+        }))
+        .then((foldersContents) => {
+          resolve(foldersContents.reduce((all, folderContents) => all.concat(folderContents), []));
+        });
+      });
+    });
+  }
+
+if (process.argv[2] === "deploy") {
+    walk(kb_path)
+    .then(files => {
+        let formData = new FormData();
+        files.map(filePath => {
+            const fileKey = filePath.substring(kb_path.length+1);
+            formData.append(fileKey, fs.createReadStream(filePath));
+        })
+        const formHeaders = formData.getHeaders();
+        return axios.put(config.KB_HOST+"/deploy", formData, {
+            headers: {
+                ...formHeaders
+            }
+        })
     }).then(res => {
-        console.log(res);
+        console.log("Successfully deploy knowledgebase. Details:\n");
+        console.log(res.data);
     }).catch(err => {
-        console.log(err);
+        console.log(err.message);
     })
 }
