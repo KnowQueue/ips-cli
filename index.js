8b573a5c641e3b0480add185663e44048b959d14
Phuoc Do <binanhphuoc@gmail.com>
2019-12-31 02:35:10 -0500

    Add commander

--- index.js
+++ index.js
@@ -3,43 +3,63 @@
 const path = require('path');
 const fs = require('fs');
 const axios = require('axios');
+const program = require('commander');
 var FormData = require('form-data');
 
 const working_dir = process.cwd();
 const config_path = path.join(working_dir, "knowq.config");
+if (!fs.existsSync(config_path)) {
+  console.log("Not a KnowQueue-initialized directory.");
+  console.log("Run \"knowq init\" to initialize this directory.");
+  process.exit(1);
+}
+
 let config = JSON.parse(fs.readFileSync(config_path));
 
-let kb_path = path.isAbsolute(config.KB_MODEL) ? config.KB_MODEL : path.join(working_dir, config.KB_MODEL);
-
 function walk(dir) {
-    return new Promise((resolve, reject) => {
-      fs.readdir(dir, (error, files) => {
-        if (error) {
-          return reject(error);
-        }
-        Promise.all(files.map((file) => {
-          return new Promise((resolve, reject) => {
-            const filepath = path.join(dir, file);
-            fs.stat(filepath, (error, stats) => {
-              if (error) {
-                return reject(error);
-              }
-              if (stats.isDirectory()) {
-                walk(filepath).then(resolve);
-              } else if (stats.isFile()) {
-                resolve(filepath);
-              }
-            });
+  return new Promise((resolve, reject) => {
+    fs.readdir(dir, (error, files) => {
+      if (error) {
+        return reject(error);
+      }
+      Promise.all(files.map((file) => {
+        return new Promise((resolve, reject) => {
+          const filepath = path.join(dir, file);
+          fs.stat(filepath, (error, stats) => {
+            if (error) {
+              return reject(error);
+            }
+            if (stats.isDirectory()) {
+              walk(filepath).then(resolve);
+            } else if (stats.isFile()) {
+              resolve(filepath);
+            }
           });
-        }))
-        .then((foldersContents) => {
-          resolve(foldersContents.reduce((all, folderContents) => all.concat(folderContents), []));
         });
+      }))
+      .then((foldersContents) => {
+        resolve(foldersContents.reduce((all, folderContents) => all.concat(folderContents), []));
       });
     });
-  }
+  });
+}
 
-if (process.argv[2] === "deploy") {
+program
+  .name('knowq')
+  .usage("command [options]")
+
+program
+  .command('init')
+  .description('initialize a KnowQueue directory.')
+  .action(() => {
+    console.log("init");
+  })
+
+program
+  .command('deploy')
+  .description('deploy to a running KnowQueue server.')
+  .action(() => {
+    let kb_path = path.isAbsolute(config.KB_MODEL) ? config.KB_MODEL : path.join(working_dir, config.KB_MODEL);
     walk(kb_path)
     .then(files => {
         let formData = new FormData();
@@ -54,9 +74,15 @@ if (process.argv[2] === "deploy") {
             }
         })
     }).then(res => {
-        console.log("Successfully deploy knowledgebase. Details:\n");
+        console.log("Successfully deploy knowledgebase.\n");
         console.log(res.data);
     }).catch(err => {
         console.log(err.message);
     })
+  })
+
+program.parse(process.argv)
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
 }
